3616178ca3b85a8b0657b556427ffcb62a985ebc
Shebanti Haldar <shebantih@gmail.com>
2025-07-14 20:22:40 +0530

    Window in openGL + Triangle

--- I_hate_OPENGL/main.cpp
+++ I_hate_OPENGL/main.cpp
@@ -2,7 +2,61 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main() {
 
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLfloat vetices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f, 
+		 0.0f,  0.5f, 0.0f  
+	};
+
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Window", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, 800, 800);
+	glClearColor(0.58f, 0.0f, 0.88f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while(!glfwWindowShouldClose(window)) {
+
+		glfwPollEvents();
+	}
+	
+
+	glfwDestroyWindow(window);
+
+
+	glfwTerminate();
 	return 0;
 }
